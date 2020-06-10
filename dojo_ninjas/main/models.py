7b1ff8492c7a5fad17f7cf0528ea4c22f6222973
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-10 13:00:22 -0700

    Fixed Charfield errors

--- dojo_ninjas/main/models.py
+++ dojo_ninjas/main/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Dojo(models.Model):
     name = models.CharField(max_length=255)
-    city = models.Charfield(max_length=255)
-    state = models.Charfield(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
